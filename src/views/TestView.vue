5e73bd6f002525da9c64ba05cdf68337c4cddaae
nsid <ismaelngassa730@gmail.com>
2022-12-15 11:24:28 +0100

    mon convertisseur de devise XAF en autre devise

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,14 +1,80 @@
 <template>
-    <div class="about">
-      <h1>Yes nsid jjjjjjjjjjjjjj</h1>
-      <label> Nom </label> <input type="text">
+  <div class="about">
+    <h1>CONVERTISEUR DE DEVISE</h1>
+    <br/>
+    <br/>
+    <div class="formualire">
       <br/>
-      <label> Nom </label> <input type="text">
+      <p>
+        <form>
+
+        </form>
+          <label> Unité à convertir </label> <input type="text" v-model="valeurdepart">
+          <select v-model="unitedepart">
+            <option value="XAF">XAF</option>
+            <option value="EUR">EUR</option>
+            <option value="USD">USD</option>
+            <option value="GBP">GBP</option>
+          </select>
+          <br/>
+          <br/>
+          <label> Resultat </label> <input type="text" v-model="resultat">
+          <select v-model="unitearrive">
+            <option value="XAF">XAF</option>
+            <option value="EUR">EUR</option>
+            <option value="USD">USD</option>
+            <option value="GBP">GBP</option>
+          </select>
+          <br/>
+          <br/>
+          <button @click="convertir"> Convertir </button>
+          <br/>
+          <br/>
+        {{ phrase }}
+      </p>
+        
     </div>
+  </div>
+ 
+</template>
+
+<script lang="ts">
+  export default {
+
+    data(){
+      return {
+        msg : "bonjour",
+        valeurdepart:0,
+        phrase :"",
+        resultat:0,
+        unitedepart:"",
+        unitearrive:"",
+
+        
+      }
+    },
+    methods:{
+      ajouter(){
+        this.phrase = " Conversion réussie !"
+      },
+      convertir(){
+      if(this.unitedepart== "XAF" && this.unitearrive =="EUR") 
+      {
+        this.resultat = this.valeurdepart * 650;
+      }
+      if(this.unitedepart== "XAF" && this.unitearrive =="USD") 
+      {
+        this.resultat = this.valeurdepart * 630;
+      }
+      if(this.unitedepart== "XAF" && this.unitearrive =="GBP") 
+      {
+        this.resultat = this.valeurdepart * 1000;
+      }
+    }
     
-      
-    
-  </template>
+    }
+  }
+</script>
 
   <style>
   @media (min-width: 1024px) {
